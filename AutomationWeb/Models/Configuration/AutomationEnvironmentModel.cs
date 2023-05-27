40c9d3e02d42a3d83fffab47bc5c8288af0dff56
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-27 14:04:35 +0300

    Added models for existing configuration sections

---
+++ AutomationWeb/Models/Configuration/AutomationEnvironmentModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AutomationWeb.Models.Configuration;
+
+public sealed class AutomationEnvironmentModel
+{
+    public string Endpoint { get; set; }
+    public string EnvironmentType { get; set; }
+}
