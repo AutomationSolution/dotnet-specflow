9cf184993676e5aa34381df964c41d65d50a3b82
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-05-27 14:04:35 +0300

    Added models for existing configuration sections

---
+++ AutomationWeb/Models/Configuration/AutomationLoggingModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomationWeb.Models.Configuration;
+
+public class AutomationLoggingModel
+{
+    public LogLevel LogLevel { get; set; }
+}
+
+public class LogLevel
+{
+    public string Default { get; set; }
+    public string System { get; set; }
+    public string Microsoft { get; set; }
+}
