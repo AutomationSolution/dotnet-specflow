53aac5cec4fa55e8f2553eab5c5b6e7f6cdf0bf8
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-05-27 14:05:05 +0300

    Added class to perform automation configuration

---
+++ AutomationWeb/Configuration/AutomationConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using AutomationWeb.Models.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace AutomationWeb.Configuration;
+
+public static class AutomationConfiguration
+{
+    public static AutomationEnvironmentModel AutomationEnvironmentModel { get; private set; }
+    public static AutomationLoggingModel AutomationLoggingModel { get; private set; }
+    public static ConfigurationManager ConfigurationManagerInstance { get; private set; }
+    
+    public static void InitializeAutomationConfiguration()
+    {
+        // Init Configuration manager
+        ConfigurationManagerInstance = new ConfigurationManager();
+
+        // Add sources
+        ConfigurationManagerInstance.AddJsonFile(Path.Combine("appsettings.json"));
+        ConfigurationManagerInstance.AddJsonFile(Path.Combine("Resources", "Configuration", "environment.json"));
+        // x.AddUserSecrets();  // TODO implement
+        ConfigurationManagerInstance.AddEnvironmentVariables();
+
+        // Bind settings from configuration manager to a model
+        AutomationEnvironmentModel = ConfigurationManagerInstance.GetSection("environment").Get<AutomationEnvironmentModel>();
+        AutomationLoggingModel = ConfigurationManagerInstance.GetSection("Logging").Get<AutomationLoggingModel>();
+    }
+}
