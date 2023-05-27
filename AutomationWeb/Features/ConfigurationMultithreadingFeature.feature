83102f5b4d47b80ef62609a57426f916c26f3c9e
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-27 18:45:32 +0300

    Added same test to 2 features for parallelization testing

---
+++ AutomationWeb/Features/ConfigurationMultithreadingFeature.feature
@@ -0,0 +1,10 @@
+﻿Feature: ConfigurationMultithreadingFeature
+  Test multiple configuration sources and it's content
+
+@ParallelizationTesting
+@SolutionTesting
+@ConfigurationFeature
+Scenario: Time is set correctly
+  Given The GUID is set in Configuration for specific scenario
+  When I wait for '5' seconds
+  Then I assert that GUID set in Configuration is the same
--- AutomationWeb/StepDefinitions/ConfigurationFeatureStepDefinitions.cs
+++ AutomationWeb/StepDefinitions/ConfigurationFeatureStepDefinitions.cs
@@ -1,6 +1,8 @@
 ﻿using AutomationWeb.Configuration;
+using FluentAssertions;
 using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using Microsoft.Extensions.Configuration.Json;
+using NLog;
 using TechTalk.SpecFlow;
 
 namespace AutomationWeb.StepDefinitions;
@@ -8,6 +10,13 @@ namespace AutomationWeb.StepDefinitions;
 [Binding]
 public class ConfigurationFeatureStepDefinitions
 {
+    private readonly ScenarioContext scenarioContext;
+    
+    public ConfigurationFeatureStepDefinitions(ScenarioContext scenarioContext)
+    {
+        this.scenarioContext = scenarioContext;
+    }
+    
     [Then(@"I assert that '(.*)' file contains in ConfigurationManager sources")]
     public void ThenIAssertThatFileContainsInConfigurationManagerSources(string fileName)
     {
@@ -20,5 +29,26 @@ public class ConfigurationFeatureStepDefinitions
     {
         var sourcesList = AutomationConfiguration.ConfigurationManagerInstance.Sources.ToList();
         sourcesList.Single(x => x.GetType() == typeof(EnvironmentVariablesConfigurationSource));
+    }
+
+    [Given(@"The GUID is set in Configuration for specific scenario")]
+    public void GivenTheGuidIsSetInConfigurationForSpecificScenario()
+    {
+        var guid = Guid.NewGuid().ToString();
+        AutomationConfiguration.TestThreadScopedModel.guid = guid;
+        scenarioContext.Set(guid, "guid");
+    }
+
+    [When(@"I wait for '(\d+)' seconds")]
+    public void WhenIWaitForSeconds(int secondsToWait)
+    {
+        Thread.Sleep(TimeSpan.FromSeconds(secondsToWait));
+    }
+
+    [Then(@"I assert that GUID set in Configuration is the same")]
+    public void ThenIAssertThatGuidSetInConfigurationIsTheSame()
+    {
+        var expectedGuid = scenarioContext.Get<string>("guid");
+        AutomationConfiguration.TestThreadScopedModel.guid.Should().Be(expectedGuid);
     }
 }
