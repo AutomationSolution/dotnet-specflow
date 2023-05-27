316cd60fdf0329db35f9c07c8fb07afb836eab50
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-27 14:05:55 +0300

    Added configuration execution in BeforeTestRun hook

---
+++ AutomationWeb/Hooks/BaseHooks.cs
@@ -0,0 +1,15 @@
+﻿using AutomationWeb.Configuration;
+using BoDi;
+using TechTalk.SpecFlow;
+
+namespace AutomationWeb.Hooks;
+
+[Binding]
+public class BaseHooks
+{
+    [BeforeTestRun(Order = 10)]
+    public static void BeforeTestRunInitConfiguration(IObjectContainer objectContainer)
+    {
+        AutomationConfiguration.InitializeAutomationConfiguration();
+    }
+}
