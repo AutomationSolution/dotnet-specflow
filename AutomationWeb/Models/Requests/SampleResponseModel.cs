8bf401952ad386d076b7d51752cc49ae9c329848
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-27 14:23:30 +0300

    Added sample REST response model

---
+++ AutomationWeb/Models/Requests/SampleResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace AutomationWeb.Models.Requests;
+
+public class SampleResponseModel
+{
+    [JsonProperty("_id")]
+    public string Id { get; set; }
+
+    [JsonProperty("_name")]
+    public string Name { get; set; }
+}
