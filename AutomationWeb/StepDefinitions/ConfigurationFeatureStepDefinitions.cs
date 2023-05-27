27af16b28b0d89a1b31d1ea77d3156848d829127
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-05-27 14:05:31 +0300

    Added step definition for configuration testing feature

---
+++ AutomationWeb/StepDefinitions/ConfigurationFeatureStepDefinitions.cs
@@ -0,0 +1,16 @@
+﻿using AutomationWeb.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using TechTalk.SpecFlow;
+
+namespace AutomationWeb.StepDefinitions;
+
+[Binding]
+public class ConfigurationFeatureStepDefinitions
+{
+    [Then(@"I assert that '(.*)' file contains in ConfigurationManager sources")]
+    public void ThenIAssertThatFileContainsInConfigurationManagerSources(string fileName)
+    {
+        var sourcesList = AutomationConfiguration.ConfigurationManagerInstance.Sources.ToList();
+        sourcesList.Single(x => x.GetType() == typeof(JsonConfigurationSource) && (x as JsonConfigurationSource).Path.Contains(fileName));
+    }
+}
