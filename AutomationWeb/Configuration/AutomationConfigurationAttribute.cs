9e44b98354b782cbc49130c1bb36e6a0d97094e2
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-05-28 12:45:24 +0300

    Added AutomationConfigurationAttribute to store properties from PropertyGroup in .csproj

---
+++ AutomationWeb/Configuration/AutomationConfigurationAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace AutomationWeb.Configuration;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+sealed class AutomationConfigurationAttribute : Attribute
+{
+    public string AutomationEnvironment { get; }
+    public string UserSecretsId { get; }
+    
+    public AutomationConfigurationAttribute(string automationEnvironment, string userSecretsId)
+    {
+        AutomationEnvironment = automationEnvironment;
+        UserSecretsId = userSecretsId;
+    }
+}
