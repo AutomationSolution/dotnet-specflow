73476d251b6e45b980cc91635aef25303bb69d64
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-05-28 15:12:27 +0300

    Moved core configuration code to AutomationFramework project

---
+++ AutomationFramework/Configuration/AutomationConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using AutomationFramework.Models.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace AutomationFramework.Configuration;
+
+/// <summary>
+/// Class to keep all configuration settings at one place for all dependent projects.
+/// </summary>
+public class AutomationConfiguration
+{
+    private static ConfigurationManager? ConfigurationManagerInstance;
+    
+    // Objects accessible within AutomationFramework project and all dependent projects
+    // If needed, other projects can use it's own objects by binding them from Instance 
+    public static RuntimeConfigurationModel RuntimeConfigurationModel { get; private set; }
+    public static LoggingModel LoggingModel { get; private set; }
+
+    public static ConfigurationManager Instance
+    {
+        get
+        {
+            if (ConfigurationManagerInstance is null)
+            {
+                ConfigurationManagerInstance = new ConfigurationManager();
+                AddDefaultStaticSources();
+            }
+
+            return ConfigurationManagerInstance;
+        }
+    }
+    
+    private const string AppSettingsFileName = "appsettings.json";
+
+    /// <summary>
+    /// Sources required for all projects which will use this class as configuration
+    /// </summary>
+    private static void AddDefaultStaticSources()
+    {
+        ConfigurationManagerInstance.AddJsonFile(AppSettingsFileName);
+        RuntimeConfigurationModel = ConfigurationManagerInstance.GetRequiredSection("RuntimeConfiguration").Get<RuntimeConfigurationModel>(); // Partial load to use correct environment.json file
+    }
+}
---
+++ AutomationWeb/Configuration/AutomationWebConfiguration.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection;
+using AutomationFramework.Configuration;
+using AutomationWeb.Models.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace AutomationWeb.Configuration;
+
+public static class AutomationWebConfiguration
+{
+    public static EnvironmentModel EnvironmentModel { get; private set; }
+    public static SecretsModel SecretsModel { get; private set; }
+    public static ProjectPropertiesAttribute ProjectProperties { get; private set; }
+
+    [field: ThreadStatic] public static TestThreadScopedModel TestThreadScopedModel { get; private set; }
+
+    private const string EnvironmentFileName = "environment.json";
+    private const string EnvironmentFormattedFileName = "environment.{0}.json";
+    private static readonly string ConfigurationResourcesPath = Path.Combine("Resources", "Configuration");
+
+    private static void AddStaticSources()
+    {
+        // environment.json + environment.DOTNETCORE_ENVIRONMENT.json
+        AutomationConfiguration.Instance.AddJsonFile(Path.Combine(ConfigurationResourcesPath, EnvironmentFileName));
+        AutomationConfiguration.Instance.AddJsonFile(
+            Path.Combine(ConfigurationResourcesPath, string.Format(EnvironmentFormattedFileName, AutomationConfiguration.RuntimeConfigurationModel.AutomationEnvironment)),
+            optional: true);
+
+        // CMD args
+        AutomationConfiguration.Instance.AddCommandLine(Environment.GetCommandLineArgs());
+
+        // User secrets
+        AutomationConfiguration.Instance.AddUserSecrets(Assembly.GetCallingAssembly(), optional: false);    // Secrets stored locally
+
+        // Environment variables
+        AutomationConfiguration.Instance.AddEnvironmentVariables();
+    }
+
+    /// <summary>
+    /// Use with static configuration Properties. Call in [BeforeTestRun] hook.
+    /// </summary>
+    public static void InitTestRunConfiguration()
+    {
+        AddStaticSources();
+
+        // Bind necessary static objects
+        EnvironmentModel = AutomationConfiguration.Instance.GetRequiredSection("Environment").Get<EnvironmentModel>();
+        SecretsModel = AutomationConfiguration.Instance.GetRequiredSection("Secrets").Get<SecretsModel>();
+        ProjectProperties = Assembly.GetCallingAssembly().GetCustomAttribute<ProjectPropertiesAttribute>();
+    }
+
+    private static void AddThreadStaticSources()
+    {
+        // If you have some env variable(or config value), which is set after InitTestRunConfiguration method, you need to manually add dependent resource here
+        
+        Environment.SetEnvironmentVariable("template:time", "now");    // TODO delete or refactor
+        Environment.SetEnvironmentVariable("template:guid", "00000000-0000-0000-0000-000000000000");    // TODO delete or refactor
+        AutomationConfiguration.Instance.AddEnvironmentVariables();    // TODO delete or refactor
+    }
+
+    /// <summary>
+    /// Use with ThreadStatic configuration Properties (Test Thread scoped). Call in [BeforeScenario] hook
+    /// </summary>
+    public static void InitTestThreadConfiguration()
+    {
+        AddThreadStaticSources();
+
+        TestThreadScopedModel = AutomationConfiguration.Instance.GetSection("template").Get<TestThreadScopedModel>();
+    }
+}
