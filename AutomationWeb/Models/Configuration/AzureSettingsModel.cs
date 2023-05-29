41d80ddf0b8bc6466b4e5883a3a52872dd832355
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-29 10:24:15 +0300

    Azure key vault update

--- AutomationWeb/Models/Configuration/AzureSettingsModel.cs
+++ AutomationWeb/Models/Configuration/AzureSettingsModel.cs
@@ -4,4 +4,7 @@ public class AzureSettingsModel
 {
     public string KeyVaultName { get; set; }
     public string KeyVaultEndPoint { get; set; }
+    public Guid TenantId { get; set; }
+    public Guid ClientId { get; set; }
+    public Guid ClientSecret { get; set; }
 }
