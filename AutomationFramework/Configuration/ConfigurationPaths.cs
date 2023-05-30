1f3ceb4a35c6f7efb84b753ea14d40de147319aa
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-05-30 10:53:09 +0300

    Added ConfigurationPaths

---
+++ AutomationFramework/Configuration/ConfigurationPaths.cs
@@ -0,0 +1,8 @@
+﻿namespace AutomationFramework.Configuration;
+
+public static class ConfigurationPaths
+{
+    public const string ResourcesDirectoryName = "Resources";
+    public const string ConfigurationDirectoryName = "Configuration";
+    public const string TestDataDirectoryName = "TestData";
+}
