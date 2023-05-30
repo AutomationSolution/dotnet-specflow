a57ad3e2b07ffc1256124d6f58f37ab1a5a00cf2
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-05-30 20:29:37 +0300

    Added EnumUtils.cs

---
+++ AutomationFramework/Utilities/EnumUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace AutomationFramework.Utilities;
+
+public static class EnumUtils
+{
+    private static readonly Random Random = new();
+
+    public static T GetRandomEnumValue<T>()
+    {
+        var values = Enum.GetValues(typeof(T));
+        return (T) values.GetValue(Random.Next(values.Length));
+    }
+}
