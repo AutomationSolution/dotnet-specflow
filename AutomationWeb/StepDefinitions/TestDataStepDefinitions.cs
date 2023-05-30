152a93c0cd1b9ef65bb8f69a4298bd7052ffe21f
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-30 10:52:54 +0300

    Added UserData.json, it's handling via Reflection and tests to it

---
+++ AutomationWeb/StepDefinitions/TestDataStepDefinitions.cs
@@ -0,0 +1,18 @@
+﻿using AutomationWeb.Enums.TestData;
+using AutomationWeb.Models.TestData;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace AutomationWeb.StepDefinitions;
+
+[Binding]
+public class TestDataStepDefinitions
+{
+    [Then(@"I assert that '(.*)' does have '(.*)' username, '(.*)' status and '(.*)' verification")]
+    public void ThenIAssertThatDoesHaveUsernameStatusAndVerification(UserDataModel userDataModel, string username, UserStatus status, UserVerification verification)
+    {
+        userDataModel.Username.Should().Be(username);
+        userDataModel.Status.Should().Be(status);
+        userDataModel.Verification.Should().Be(verification);
+    }
+}
---
+++ AutomationWeb/Utilities/FrameworkAdditions/SpecFlow/CustomStepArgumentTransformations.cs
@@ -0,0 +1,21 @@
+﻿using AutomationWeb.Configuration;
+using AutomationWeb.Models.TestData;
+using TechTalk.SpecFlow;
+
+namespace AutomationWeb.Utilities.FrameworkAdditions.SpecFlow;
+[Binding]
+public class CustomStepArgumentTransformations
+{
+    [StepArgumentTransformation(@"(.*)")]
+    public UserDataModel UserDataModelFromUserAliasTransform(string userAlias)
+    {
+        try
+        {
+            return AutomationWebConfiguration.UsersDataModel.GetType().GetProperty(userAlias).GetValue(AutomationWebConfiguration.UsersDataModel, null) as UserDataModel;
+        }
+        catch (NullReferenceException e)
+        {
+            throw new ArgumentOutOfRangeException(userAlias, $"Can't find a specified property in {nameof(AutomationWebConfiguration.UsersDataModel)} object");
+        }
+    }
+}
