1165fea66772bd5aebb34a8f3939aac4919255a5
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-05-31 09:01:03 +0300

    Added feature and scenario to test layout renderer

---
+++ AutomationMobile/StepDefinitions/LoggerStepDefinitions.cs
@@ -0,0 +1,28 @@
+﻿using AutomationMobile.Configuration;
+using FluentAssertions;
+using NLog;
+using TechTalk.SpecFlow;
+
+namespace AutomationMobile.StepDefinitions;
+
+[Binding]
+public class LoggerStepDefinitions
+{
+    [Then(@"I assert that layout renderer is set up correctly")]
+    public void ThenIAssertThatLayoutRendererIsSetUpCorrectly()
+    {
+        // Do everything in one step to not capture SpecFlow logs
+        // Arrange
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        // Act
+        LogManager.GetCurrentClassLogger().Warn($"Custom message");
+
+        // Assert
+        var consoleOutput = stringWriter.ToString();
+        consoleOutput.Should().Contain(AutomationMobileConfiguration.DeviceConfigModel.ApplicationName.ToString());
+        consoleOutput.Should().Contain("WARN");
+        consoleOutput.Should().Contain("Custom message");
+    }
+}
