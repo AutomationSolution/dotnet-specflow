f7b47034491178d5f5693bef9aa40bd50cd0ce31
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-31 08:35:28 +0300

    Added CustomLayoutRenderers.cs

---
+++ AutomationMobile/Utilities/NLog/CustomLayoutRenderers.cs
@@ -0,0 +1,22 @@
+﻿using AutomationMobile.Configuration;
+using NLog;
+
+namespace AutomationMobile.Utilities.NLog;
+
+public static class CustomLayoutRenderers
+{
+    public static void AddApplicationNameLayoutRenderer()
+    {
+        LogManager.Setup().SetupExtensions(builder => builder.RegisterLayoutRenderer("applicationName", _ =>
+        {
+            try
+            {
+                return AutomationMobileConfiguration.DeviceConfigModel.ApplicationName;
+            }
+            catch (NullReferenceException)
+            {
+                return string.Empty;
+            }
+        }));
+    }
+}
