a2fb23b2a4dabd7fa8ac0a0bf8f50face744a7ff
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-31 09:00:41 +0300

    Added init of layout renderers in hooks

---
+++ AutomationMobile/Hooks/LoggerHooks.cs
@@ -0,0 +1,14 @@
+﻿using AutomationMobile.Utilities.NLog;
+using TechTalk.SpecFlow;
+
+namespace AutomationMobile.Hooks;
+
+[Binding]
+public class LoggerHooks
+{
+    [BeforeTestRun(Order = 50)]
+    public static void BeforeTestRunInitLoggerLayoutRenderers()
+    {
+        CustomLayoutRenderers.AddApplicationNameLayoutRenderer();
+    }
+}
