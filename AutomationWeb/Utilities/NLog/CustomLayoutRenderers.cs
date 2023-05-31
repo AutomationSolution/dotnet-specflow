57c2c680f55d873f2d2af64ebac7d54683182b65
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-05-31 08:34:01 +0300

    Added custom Layout renderer for web project

---
+++ AutomationWeb/Utilities/NLog/CustomLayoutRenderers.cs
@@ -0,0 +1,22 @@
+﻿using AutomationWeb.Configuration;
+using NLog;
+
+namespace AutomationWeb.Utilities.NLog;
+
+public static class CustomLayoutRenderers
+{
+    public static void AddScenarioGuidRenderer()
+    {
+        LogManager.Setup().SetupExtensions(builder => builder.RegisterLayoutRenderer("scenarioGuid", _ =>
+        {
+            try
+            {
+                return AutomationWebConfiguration.ScenarioMetaData.ScenarioGuid;
+            }
+            catch (NullReferenceException)
+            {
+                return string.Empty;
+            }
+        }));
+    }
+}
