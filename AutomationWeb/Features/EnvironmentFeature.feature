babe7f9abd2aa218a2e2e9306f2211f1ee67bbbc
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-06-01 09:58:43 +0300

    Added scenario to test configuration sources priority

---
+++ AutomationWeb/Features/EnvironmentFeature.feature
@@ -0,0 +1,21 @@
+﻿Feature: EnvironmentFeature
+
+  @SolutionTesting
+  @ConfigurationFeature
+  Scenario: Environment variables are taken into priority over other sources in configuration
+    Given 'featureTest.json' file is added as a source to ConfigurationManager
+      And 'Priority' section is mapped to a MappedSectionObject object
+      And Mapped object contain the following environment variables
+        | VariableSection | VariableName | VariableValue     |
+        | Priority        | Name         | NameValueFile     |
+        | Priority        | Password     | PasswordValueFile |
+      And The following environment variables are set
+        | VariableSection | VariableName | VariableValue     |
+        | Priority        | Name         | NameValueStep     |
+        | Priority        | Password     | PasswordValueStep |
+      And Environment variables are added as a source to ConfigurationManager
+      And 'Priority' section is mapped to a MappedSectionObject object
+    Then I assert that mapped object contain the following environment variables
+      | VariableSection | VariableName | VariableValue     |
+      | Priority        | Name         | NameValueStep     |
+      | Priority        | Password     | PasswordValueStep |
---
+++ AutomationWeb/StepDefinitions/EnvironmentStepDefinitions.cs
@@ -0,0 +1,79 @@
+﻿using AutomationFramework.Configuration;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using TechTalk.SpecFlow.Assist.Attributes;
+using static AutomationFramework.Configuration.ConfigurationPaths;
+
+namespace AutomationWeb.StepDefinitions;
+
+[Binding]
+public class EnvironmentStepDefinitions
+{
+    private readonly ScenarioContext scenarioContext;
+
+    public EnvironmentStepDefinitions(ScenarioContext scenarioContext)
+    {
+        this.scenarioContext = scenarioContext;
+    }
+
+    [Given(@"The following environment variables are set")]
+    public void GivenTheFollowingEnvironmentVariablesAreSet(Table table)
+    {
+        var environmentVariables = table.CreateSet<SimpleEnvironmentVariable>();
+        foreach (var variable in environmentVariables)
+        {
+            Environment.SetEnvironmentVariable($"{variable.Section}:{variable.Name}", variable.Value);
+        }
+    }
+
+    private class SimpleEnvironmentVariable
+    {
+        [TableAliases("VariableSection")]
+        public string Section { get; set; }
+
+        [TableAliases("VariableName")]
+        public string Name { get; set; }
+
+        [TableAliases("VariableValue")]
+        public string Value { get; set; }
+    }
+
+    private class MappedSectionObject
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+
+    [Given(@"Environment variables are added as a source to ConfigurationManager")]
+    public void GivenEnvironmentVariablesAreAddedAsASourceToConfigurationManager()
+    {
+        AutomationConfigurationManager.Instance.AddEnvironmentVariables();
+    }
+
+    [Given(@"'(.*)' section is mapped to a MappedSectionObject object")]
+    public void GivenSectionIsMappedToAMappedSectionObjectObject(string sectionName)
+    {
+        var envObjectFromConfiguration = AutomationConfigurationManager.Instance.GetRequiredSection(sectionName).Get<MappedSectionObject>();
+        scenarioContext.Set(envObjectFromConfiguration);
+    }
+
+    [Given("Mapped object contain the following environment variables")]
+    [Then(@"I assert that mapped object contain the following environment variables")]
+    public void ThenIAssertThatMappedObjectContainTheFollowingEnvironmentVariables(Table table)
+    {
+        var actualResult = scenarioContext.Get<MappedSectionObject>();
+
+        var expectedResult = table.CreateSet<SimpleEnvironmentVariable>();
+
+        expectedResult.Any(x => x.Value == actualResult.Name).Should().BeTrue();
+        expectedResult.Any(x => x.Value == actualResult.Password).Should().BeTrue();
+    }
+
+    [Given(@"'(.*)' file is added as a source to ConfigurationManager")]
+    public void GivenFileIsAddedAsASourceToConfigurationManager(string fileName)
+    {
+        AutomationConfigurationManager.Instance.AddJsonFile(Path.Combine(ResourcesDirectoryName, ConfigurationDirectoryName, fileName));
+    }
+}
