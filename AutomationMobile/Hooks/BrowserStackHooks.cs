fa148d3d56bee1188b2e7583e231a4c9e18ca6a0
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-06-02 00:37:55 +0300

    BrowserStack scripts update

---
+++ AutomationMobile/Hooks/BrowserStackHooks.cs
@@ -0,0 +1,28 @@
+﻿using AutomationMobile.Utilities.BrowserStack;
+using TechTalk.SpecFlow;
+
+namespace AutomationMobile.Hooks;
+
+[Binding]
+public class BrowserStackHooks
+{
+    private readonly BrowserStackExecutor browserStackExecutor = new();
+    
+    [BeforeStep(Order = 1)]
+    public void LogBrowserStackBeforeStep(ScenarioContext scenarioContext)
+    {
+        browserStackExecutor.AddAnnotation($"Specflow step started: {scenarioContext.StepContext.StepInfo.Text}");
+    }
+    
+    [AfterStep(Order = 1)]
+    public void LogBrowserStackAfterStep(ScenarioContext scenarioContext)
+    {
+        browserStackExecutor.AddAnnotation($"Specflow step finished: {scenarioContext.StepContext.StepInfo.Text}");
+    }
+    
+    [AfterScenario(Order = 1)]
+    public void SetBrowserStackTestStatus(ScenarioContext scenarioContext)
+    {
+        browserStackExecutor.ChangeTestStatus(scenarioContext.ScenarioExecutionStatus, scenarioContext.TestError?.Message + scenarioContext.TestError?.StackTrace);
+    }
+}
---
+++ AutomationMobile/Utilities/BrowserStack/BrowserStackScriptsFormatter.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AutomationMobile.Utilities.BrowserStack;
+
+// Whole this class looks like a mess, but haven't found a better solution among existing ones
+public static class BrowserStackScriptsFormatter
+{
+    private static string doubleQuotes = "\"";
+    private static string bracketOpen = "{";
+    private static string bracketClose = "}";
+    private static string tempPlaceholderDoubleQuotes = "__TEMP__";
+    private static string tempPlaceholderBracketOpen = "__BRACKETOPEN__";
+    private static string tempPlaceholderBracketClose = "__BRACKETCLOSE__";
+
+    private static string pattern = @"{\d+}";
+    private static string patternRestore = @"<<\d+>>";
+
+    public static string ReplaceBeforeFormat(string input)
+    {
+        // Securing our targets to perform formatting
+        var result = Regex.Replace(input, pattern, FormattingReplacerBefore);
+        // Replacing conflicting symbols before formatting
+        result = result
+            .Replace(doubleQuotes, tempPlaceholderDoubleQuotes)
+            .Replace(bracketOpen, tempPlaceholderBracketOpen)
+            .Replace(bracketClose, tempPlaceholderBracketClose);
+        // Restoring out targets fo perform formatting
+        result = Regex.Replace(result, patternRestore, FormattingReplacerAfter);
+        return result;
+    }
+
+    public static string ReplaceAfterFormat(string input)
+    {
+        return input
+            .Replace(tempPlaceholderDoubleQuotes, doubleQuotes)
+            .Replace(tempPlaceholderBracketOpen, bracketOpen)
+            .Replace(tempPlaceholderBracketClose, bracketClose);
+    }
+
+    private static string FormattingReplacerBefore(Match match)
+    {
+        return match.Value.Replace("{", "<<").Replace("}", ">>");
+    }
+
+    private static string FormattingReplacerAfter(Match match)
+    {
+        return match.Value.Replace("<<", "{").Replace(">>", "}");
+    }
+}
