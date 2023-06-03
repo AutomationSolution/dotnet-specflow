914d22a1c1eb0c8bc933b49ba064d3ef92fdf4a3
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-06-03 19:09:55 +0300

    Added UserCredentialsFileName

--- AutomationFramework/Configuration/ConfigurationPaths.cs
+++ AutomationFramework/Configuration/ConfigurationPaths.cs
@@ -5,6 +5,7 @@ public static class ConfigurationPaths
     public const string EnvironmentFileName = "environment.json";
     public const string EnvironmentFormattedFileName = "environment.{0}.json";
     public const string UserDataFileName = "UserData.json";
+    public const string UserCredentialsFileName = "UsersCredentials.json";
     public const string AppSettingsFileName = "appsettings.json";
     public const string AzureKeyVaultSettingsFileName = "azureKeyVaultSettings.json";
     public const string BrowserStackSettingsFileName = "browserStackSettings.json";
