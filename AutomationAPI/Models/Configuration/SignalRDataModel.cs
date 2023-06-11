d2261c0119b5510681206c879253f6b55dc5ef41
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-06-11 10:13:04 +0300

    Changed signalR settings

---
+++ AutomationAPI/Models/Configuration/SignalRDataModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AutomationAPI.Models.Configuration;
+
+public class SignalRDataModel
+{
+    public string SignalREndpoint { get; set; }
+    public string SignalRHubPath { get; set; }
+}
