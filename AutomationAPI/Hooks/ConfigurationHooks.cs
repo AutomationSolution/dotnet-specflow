deb1255cbb0721e65a85d36ca09dec3d1d9324e8
Dmitry Legostaev <d.legostaev@a1qa.com>
2023-06-11 10:11:15 +0300

    Added AutomationAPIConfiguration.cs

---
+++ AutomationAPI/Hooks/ConfigurationHooks.cs
@@ -0,0 +1,16 @@
+﻿using AutomationAPI.Configuration;
+using AutomationFramework.Configuration;
+using BoDi;
+using TechTalk.SpecFlow;
+
+namespace AutomationAPI.Hooks;
+
+[Binding]
+public class ConfigurationHooks
+{
+    [BeforeTestRun(Order = 10)]
+    public static void BeforeTestRunInitConfiguration(IObjectContainer objectContainer)
+    {
+        objectContainer.RegisterTypeAs<AutomationAPIConfiguration, IAutomationConfiguration>();
+    }
+}
