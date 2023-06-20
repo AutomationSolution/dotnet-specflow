3d87a4807572f2e928d92f465fbc77fe3ffd7411
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-06-21 02:47:23 +0300

    Step definitions updated

--- AutomationAPI/StepDefinitions/SignalRStepDefinitions.cs
+++ AutomationAPI/StepDefinitions/SignalRStepDefinitions.cs
@@ -1,7 +1,6 @@
 ﻿using AutomationAPI.Utilities.SignalR;
 using AutomationFramework.Utilities.Polly;
 using FluentAssertions;
-using NLog;
 using TechTalk.SpecFlow;
 
 namespace AutomationAPI.StepDefinitions;
@@ -28,18 +27,10 @@ public class SignalRStepDefinitions
     public void WhenISendASignalRMessageWithNameAndValue(string messageName, string messageValue)
     {
         var signalRConnection = scenarioContext.Get<SignalRConnection>();
-        // TODO implement conditional wait for established connection, as it is connecting asynchronously, or make connection establishing synchronous
-        // ConditionalWait.WaitFor<string>(() =>
-        // {
-        //     LogManager.GetCurrentClassLogger().Debug("Code block execution. Wait for NOT NULL");
-        //     return null;
-        // }, message: "Custom message on exception 1", codePurpose: "Wait until SignalR connection is established");
-        
-        ConditionalWait.WaitForTrue(() =>
-        {
-            LogManager.GetCurrentClassLogger().Debug("Code block execution. Wait for TRUE");
-            return signalRConnection.IsConnectionEstablished();
-        }, message: "Custom message on exception 2");
+
+        ConditionalWait.WaitForTrue(() => signalRConnection.IsConnectionEstablished(), 
+            failReason: "SignalR connection is not established",
+            codePurpose: "Wait until SignalR connection is established");
         
         signalRConnection.SendMessage(messageName, messageValue).Wait();
     }
@@ -48,11 +39,17 @@ public class SignalRStepDefinitions
     public void ThenIAssertThatSignalRMessageWithNameAndValueIsReceived(string messageName, string messageValue)
     {
         var signalRConnection = scenarioContext.Get<SignalRConnection>();
-        // TODO implement conditional wait for established connection and for new messages received
-        Thread.Sleep(TimeSpan.FromSeconds(3));
 
-        signalRConnection.IsConnectionEstablished().Should().BeTrue("Connection should be established");
-        signalRConnection.GetReceivedMessages().Count.Should().BeGreaterThan(0, "Received messages should not be empty");
+        ConditionalWait.WaitForTrue(() => signalRConnection.IsConnectionEstablished(), 
+            failReason: "SignalR connection is not established",
+            codePurpose: "Wait until SignalR connection is established");
+
+        var result = ConditionalWait.WaitForPredicateAndGetResult(
+            () => signalRConnection.GetReceivedMessages().Count, 
+            i => i > 0, 
+            failReason: "Received messages from SignalR should not be empty",
+            codePurpose: "Wait until SignalR message is received");
+
         signalRConnection.GetReceivedMessages().Any(tuple => tuple.Item1.Equals(messageName) && tuple.Item2.Equals(messageValue)).Should()
             .BeTrue($"There should be a message with {messageName} name and {messageValue} value");
     }
