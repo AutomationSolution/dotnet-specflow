c8d8e20580f3a2bf9aea766159ef1d8cd4ce0e63
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-06-20 23:42:23 +0300

    Created PollyPredicates.cs

---
+++ AutomationFramework/Utilities/Polly/PollyPredicates.cs
@@ -0,0 +1,7 @@
+﻿namespace AutomationFramework.Utilities.Polly;
+
+public static class PollyPredicates
+{
+    public static Func<T, bool> IsNullPredicate<T>() => t => t == null;
+    public static readonly Func<bool, bool> IsFalsePredicate = t => t != true;
+}
