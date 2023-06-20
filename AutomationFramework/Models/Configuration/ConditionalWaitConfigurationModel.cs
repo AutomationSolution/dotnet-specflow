4303683d5026f18d19a89c01cea36762fce0de9a
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-06-21 00:16:50 +0300

    Expanded conditional wait model

--- AutomationFramework/Models/Configuration/ConditionalWaitConfigurationModel.cs
+++ AutomationFramework/Models/Configuration/ConditionalWaitConfigurationModel.cs
@@ -1,4 +1,6 @@
-﻿namespace AutomationFramework.Models.Configuration;
+﻿using Polly.Retry;
+
+namespace AutomationFramework.Models.Configuration;
 
 public class ConditionalWaitConfigurationModel
 {
@@ -6,9 +8,12 @@ public class ConditionalWaitConfigurationModel
 
     public TimeSpan Timeout { get; set; }
     public TimeSpan BackOffDelay { get; set; }
+    public double Factor { get; set; }
+    public RetryBackoffType BackoffType { get; set; }
 
     // Adding +1 to be sure that overall retry time will exceed timeout
     // Works for constant, linear and exponential timeouts with positive factors (> 1.0)
     // I find this approach is most suitable, flexible and resource optimized for ConditionalWait implementation
+    // We can add some code to handle negative factors (< 1.0), but it's not necessary
     public int RetryCount => (int) Timeout.Divide(BackOffDelay) + 1;
 }
