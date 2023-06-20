0b8dd46c661add89deb287da4e097f508b96f69d
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-06-20 21:20:39 +0300

    ConditionalWait.cs updated

--- AutomationFramework/Utilities/Polly/ConditionalWait.cs
+++ AutomationFramework/Utilities/Polly/ConditionalWait.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using Polly;
 
 namespace AutomationFramework.Utilities.Polly;
 
@@ -7,12 +8,27 @@ public static class ConditionalWait
     public static T WaitFor<T>(Func<T> condition, TimeSpan? timeout = null, TimeSpan? pollingInterval = null, string message = null,
         IList<Type> exceptionsToIgnore = null, string codePurpose = null)
     {
-        // TODO add exceptionsToIgnore handling
-
-        // Configure policy
         var waitForNotNullPolicy = PollyAutomationPolicies.GetWaitForNotNullPolicy<T>();
 
-        // Execute policy
+        return WaitForWrapper(waitForNotNullPolicy, condition, timeout, pollingInterval, message, exceptionsToIgnore, codePurpose);
+    }
+
+    public static void WaitForTrue(Func<bool> condition, TimeSpan? timeout = null, TimeSpan? pollingInterval = null, string message = null,
+        IList<Type> exceptionsToIgnore = null, string codePurpose = null)
+    {
+        var waitForTruePolicy = PollyAutomationPolicies.GetWaitForTruePolicy();
+
+        WaitForWrapper(waitForTruePolicy, condition, timeout, pollingInterval, message, exceptionsToIgnore, codePurpose);
+    }
+
+    private static T WaitForWrapper<T>(Policy<T> policy, Func<T> condition, TimeSpan? timeout = null, TimeSpan? pollingInterval = null, string message = null,
+        IList<Type> exceptionsToIgnore = null, string? codePurpose = null)
+    {
+        // TODO add exceptionsToIgnore handling
+        // TODO implement passing explicit timeouts
+        // TODO implement passing explicit polling invtervals
+
+        // Set up logging message
         var messageBeforeExecution = $"Trying to execute code in {nameof(WaitForTrue)} method. ";
         if (codePurpose != null)
         {
@@ -21,8 +37,9 @@ public static class ConditionalWait
 
         messageBeforeExecution += "Execution attempt #1";
         
+        // Execute policy
         LogManager.GetCurrentClassLogger().Debug(messageBeforeExecution);
-        var result = waitForNotNullPolicy.Execute(condition);
+        var result = policy.Execute(condition);
 
         // Assert policy
         if (result == null)
@@ -31,24 +48,5 @@ public static class ConditionalWait
         }
 
         return result;
-    }
-
-    public static void WaitForTrue(Func<bool> condition, TimeSpan? timeout = null, TimeSpan? pollingInterval = null, string message = null,
-        IList<Type> exceptionsToIgnore = null)
-    {
-        // TODO add exceptionsToIgnore handling
-
-        // Arrange policy
-        var waitForTruePolicy = PollyAutomationPolicies.GetWaitForTruePolicy();
-
-        // Act policy
-        LogManager.GetCurrentClassLogger().Debug($"Trying to execute code in {nameof(WaitForTrue)} method. Code execution attempt #1");
-        var result = waitForTruePolicy.Execute(condition);
-
-        // Assert policy
-        if (result != true)
-        {
-            throw new TimeoutException($"Result is not true but {result}. {message}");
-        }
     }
 }
