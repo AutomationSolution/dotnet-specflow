04ede09329a254a3287daa4496459bee129bd02e
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-06-23 21:11:57 +0300

    Refactored

--- AutomationFramework/Utilities/Polly/PolicyBuilderIgnoreExceptionsUtilities.cs
+++ AutomationFramework/Utilities/Polly/PolicyBuilderIgnoreExceptionsUtilities.cs
@@ -7,47 +7,35 @@ public static class PolicyBuilderUtilities
     public static PolicyBuilder<T> HandleExceptionsFromIgnoreList<T>(IList<Type>? exceptionsToIgnore)
     {
         PolicyBuilder<T> result;
-        
-        if (exceptionsToIgnore is not null && exceptionsToIgnore.Count > 0)
-        {
-            var ignoreExceptionsList = IgnoreExceptionTypes(exceptionsToIgnore);
-            result = Policy<T>
-                .Handle<Exception>(exception => ignoreExceptionsList.Any(type => type.IsInstanceOfType(exception)));
-        }
-        else
-        {
-            throw new ArgumentNullException(nameof (exceptionsToIgnore), "exceptionTypes cannot be null or empty");
-        }
+
+        CheckExceptionsList(exceptionsToIgnore);
+
+        result = Policy<T>
+            .Handle<Exception>(exception => exceptionsToIgnore!.Any(type => type.IsInstanceOfType(exception)));
 
         return result;
     }
 
-    public static PolicyBuilder<T> OrExceptionsFromIgnoreList<T>(this PolicyBuilder<T> handleResultPolicyBuilder, IList<Type>? exceptionsToIgnore)
+    public static PolicyBuilder<T> OrExceptionsFromIgnoreList<T>(this PolicyBuilder<T> handleResultPolicyBuilder,
+        IList<Type>? exceptionsToIgnore)
     {
-        if (exceptionsToIgnore is not null && exceptionsToIgnore.Count > 0)
-        {
-            var ignoreExceptionsList = IgnoreExceptionTypes(exceptionsToIgnore);
-            handleResultPolicyBuilder
-                .Or<Exception>(exception => ignoreExceptionsList.Any(type => type.IsInstanceOfType(exception)));
-        }
+        CheckExceptionsList(exceptionsToIgnore);
+
+        handleResultPolicyBuilder
+            .Or<Exception>(exception => exceptionsToIgnore!.Any(type => type.IsInstanceOfType(exception)));
 
         return handleResultPolicyBuilder;
     }
-    
-    private static List<Type> IgnoreExceptionTypes(IList<Type> exceptionsToIgnore)
+
+    private static void CheckExceptionsList(IList<Type>? exceptionsToIgnore)
     {
-        var resultedExceptionList = new List<Type>();
-        
-        if (exceptionsToIgnore == null)
-            throw new ArgumentNullException(nameof (exceptionsToIgnore), "exceptionTypes cannot be null");
+        if (exceptionsToIgnore is null || exceptionsToIgnore.Count == 0)
+            throw new ArgumentNullException(nameof(exceptionsToIgnore), "exceptionsToIgnore list cannot be null or empty");
 
         foreach (var exceptionType in exceptionsToIgnore)
         {
-            if (!typeof (Exception).IsAssignableFrom(exceptionType))
-                throw new ArgumentException("All types to be ignored must derive from System.Exception", nameof (exceptionsToIgnore));
+            if (!typeof(Exception).IsAssignableFrom(exceptionType))
+                throw new ArgumentException("All types to be ignored must derive from System.Exception", nameof(exceptionsToIgnore));
         }
-        resultedExceptionList.AddRange(exceptionsToIgnore);
-
-        return resultedExceptionList;
     }
 }
